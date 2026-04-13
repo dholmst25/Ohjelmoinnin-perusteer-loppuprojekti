3233908383aaffde5721228569f44a85e9d8bfcb
dholmst25 <daniel.holmstrom@edu.turkuamk.fi>
2026-04-13 12:55:52 +0300

    Startmenu 1.1 + Fixes

--- DanielinPerseilyt/StartMenu.py
+++ DanielinPerseilyt/StartMenu.py
@@ -1,4 +1,21 @@
 def startmenu():
-    DecisionDesc = ("1 = Add room details 2 = View all available rooms 3 = · Update stock levels (restock, sell items) 4= Search for a product by name 5 = Remove a product")
-    StartDecision = int(input(DecisionDesc))
-startmenu()
+    print("┌───────────────────────────────────────────────┐")
+    print("|1 = Add room details                           |")
+    print("|2 = View all available rooms                   |") 
+    print("|3 = · Update stock levels (restock, sell items)|") 
+    print("|4= Search for a product by name                |") 
+    print("|5 = Remove a product                           |")
+    print("└───────────────────────────────────────────────┘")
+    #Käytetään numeroita 1-5 aloittamaan eri osat ohjelmaa ja turvataan, jos käyttäjä antaa väärän numeron
+    while True:
+        try:
+            StartDecision = int(input("Enter a number (1-5)"))
+            if 1 <= StartDecision <= 5: #Käytetään numeroita 1-5 aloittamaan eri osat ohjelmaa ja turvataan, jos käyttäjä antaa väärän numeron.
+                return StartDecision
+            else:
+                print("Incorrect value given. Try again.")
+                startmenu()
+        except ValueError:
+            print("Invalid number given. Please try again.")
+choice = startmenu()
+print(f"You have selected option {choice}")
