393790b6b7b05eb08ecd4f81ef9a9e6496107f40
dholmst25 <daniel.holmstrom@edu.turkuamk.fi>
2026-04-13 12:56:43 +0300

    Startmenu 1.1 deleted useless text

--- DanielinPerseilyt/StartMenu.py
+++ DanielinPerseilyt/StartMenu.py
@@ -6,7 +6,6 @@ def startmenu():
     print("|4= Search for a product by name                |") 
     print("|5 = Remove a product                           |")
     print("└───────────────────────────────────────────────┘")
-    #Käytetään numeroita 1-5 aloittamaan eri osat ohjelmaa ja turvataan, jos käyttäjä antaa väärän numeron
     while True:
         try:
             StartDecision = int(input("Enter a number (1-5)"))
