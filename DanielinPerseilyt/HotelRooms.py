fcfa69ac6d5e51a1dee298a4d1ddeca51c8e40b3
dholmst25 <daniel.holmstrom@edu.turkuamk.fi>
2026-04-14 10:49:53 +0300

    RoomList 1.1

---
+++ DanielinPerseilyt/HotelRooms.py
@@ -0,0 +1,7 @@
+# True = Saatavana ja False = Ei saatavana, sekä hinta per yö. Eka True/False on ammiainen ja toinen on onko saatavilla.
+rooms = [
+    {"room_id": 101, "room_name":"Standard double room","rooms_beds": "Two single beds", "room_cost": 120, "room_breakfast": False, "room_availability": False}, 
+    {"room_id": 102, "room_name": "Standard family room", "rooms_beds": "Two single beds and one king size bed", "room_cost": 350, "room_breakfast": True, "room_availability": False},
+    {"room_id": 103, "room_name": "Standard single room", "rooms_beds": "Single queen size bed", "room_cost": 60, "room_breakfast": False, "room_availability": True}
+    {"room_id": 104, "room_name": "Family suite", "rooms_beds": "Three king size beds", "room_cost" 600, "room_breakfast": True, "room_availability": True}
+]:
