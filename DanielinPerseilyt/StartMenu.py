9d231f2ef37165c9218954d52687c1372f0bc4cd
dholmst25 <daniel.holmstrom@edu.turkuamk.fi>
2026-04-14 11:05:31 +0300

    StartMenu 1.1 Spelling mistakes :3

--- DanielinPerseilyt/StartMenu.py
+++ DanielinPerseilyt/StartMenu.py
@@ -2,15 +2,15 @@ def startmenu():
     print("┌───────────────────────────────────────────────┐")
     print("|1 = Add room details                           |")
     print("|2 = View all available rooms                   |") 
-    print("|3 = Update stock levels (restock, sell items)  |") 
-    print("|4 = Search for a product by name               |") 
-    print("|5 = Remove a product                           |")
+    print("|3 = Book a room                                |") 
+    print("|4 = Check-out                                  |") 
+    print("|5 = Remove room listing                        |")
     print("|6 = Exit the program                           |")
     print("└───────────────────────────────────────────────┘")
     while True:
         try:
             StartDecision = int(input("Enter a number (1-6)"))
-            if 1 <= StartDecision <= 6: #Käytetään numeroita 1-5 aloittamaan eri osat ohjelmaa ja turvataan, jos käyttäjä antaa väärän numeron.
+            if 1 <= StartDecision <= 6: #Käytetään numeroita 1-6 aloittamaan eri osat ohjelmaa ja turvataan, jos käyttäjä antaa väärän numeron.
                 return StartDecision
             else:
                 print("Incorrect value given. Try again.")
@@ -19,7 +19,7 @@ def startmenu():
 choice = startmenu()
 print(f"You have selected option {choice}")
 if choice == 1:
-    def 
+    def
 if choice == 2:
     def
 if choice == 3:
