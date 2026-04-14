c4a08481a1ee9743f948ac674d32e513152e4171
dholmst25 <daniel.holmstrom@edu.turkuamk.fi>
2026-04-14 12:30:38 +0300

    Startmenu 1.2 major bug fix by Markus

--- DanielinPerseilyt/StartMenu.py
+++ DanielinPerseilyt/StartMenu.py
@@ -15,18 +15,31 @@ def startmenu():
             else:
                 print("Incorrect value given. Try again.")
         except ValueError:
-            print("Invalid number given. Please try again.")
-choice = startmenu()
-print(f"You have selected option {choice}")
-if choice == 1:
-    def
-if choice == 2:
-    def
-if choice == 3:
-    def
-if choice == 4:
-    def
-if choice == 5:
-    def
-if choice == 6:
-    exit
+                print("Invalid number given. Please try again.")
+
+def exit_program():
+    print("Thank you for choosing us!")
+    return False
+
+while True:
+    choice = startmenu()
+
+    if choice == 1:
+        print(f"You have selected option {choice}")
+        print("Ho ho ho!")
+    elif choice == 2:
+        print(f"You have selected option {choice}")
+        print("Hahaa")
+    elif choice == 3:
+        print(f"You have selected option {choice}")
+        print("Hoo")
+    elif choice == 4:
+        print(f"You have selected option {choice}")
+        print("Sazam!")
+    elif choice == 5:
+        print(f"You have selected option {choice}")
+        print("Haha")
+    elif choice == 6:
+        print(f"You have selected option {choice}")
+        exit_program()
+        break
