a91cc9079d34975e51d9c873603ca78021197c90
markusi24 <255119525+markusi24@users.noreply.github.com>
2026-04-19 08:40:02 +0300

    Booking v0.04(?)

--- MainCodeFolder/Room_booking.py
+++ MainCodeFolder/Room_booking.py
@@ -1,5 +1,8 @@
-import HotelRooms as rooms
+import HotelRooms
 
-def book_room():
-    if rooms:
-        rooms.get("room_id")
+def book_room(room_id, rooms):
+    for room in rooms:
+        if room["room_id"] == room_id and room["room_availability"]:
+            room["room_availability"] = False
+            return "Room booked successfully!"
+    return "Room not available!"
