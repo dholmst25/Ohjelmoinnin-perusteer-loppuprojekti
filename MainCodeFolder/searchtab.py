c7d8fc672665ba7a4d1138ba00d20446e33c9bed
jonnaturkka26 <jonna.turkka@gmail.com>
2026-04-20 21:23:37 +0300

    Käyttäjän näkymä / hakukenttä

--- MainCodeFolder/searchtab.py
+++ MainCodeFolder/searchtab.py
@@ -1,35 +1,24 @@
-from searchlogic import search_rooms
+from searchlogic import search_rooms                                                  
 
-print()
-print("Hotel room search")
-print()
-print("You can only search available rooms.")
-print()
-price_input = input("Give the maximum room cost (or press enter to skip): ")
-bed_input = input("Search by bed type (or press enter to skip): ")
-room_type_input = input("Search by room type (or press enter to skip): ")
+def search_menu():
+    print()
+    print("Hotel room search by feature")
+    print()
+    print("You can now search for available rooms with defined features. Non-available rooms are not listed.")
+    print()
+    price_input = input("Give the maximum room cost (or press enter): ")               
+    bed_input = input("Search by bed type (or press enter): ")
+    room_type_input = input("Search by room type (or press enter): ")
+                                                                                    # NONE skips definition
+    max_cost = int(price_input) if price_input else None                            # Defines MAX for search if defined
+    bed_keyword = bed_input if bed_input else None                                  # Defines bed keyword if type is defined
+    room_type_keyword = room_type_input if room_type_input else None                # Defines room type keyword if type is defined
 
-# --- INPUT → VALUES ---
-max_cost = int(price_input) if price_input else None
-bed_keyword = bed_input if bed_input else None
-room_type_keyword = room_type_input if room_type_input else None
+    results = search_rooms(max_cost=max_cost, bed_keyword=bed_keyword, room_type_keyword=room_type_keyword)
 
-# --- SEARCH ---
-results = search_rooms(
-    max_cost=max_cost,
-    bed_keyword=bed_keyword,
-    room_type_keyword=room_type_keyword
-)
-
-# --- OUTPUT ---
-print("\nAvailable rooms:")
-
-if not results:
-    print("No rooms found with given criteria.")
-else:
-    for room in results:
-        print(
-            f"- {room['room_name']} | "
-            f"{room['room_cost']} € | "
-            f"{room['rooms_beds']}"
-        )
+    print("Available rooms:")
+    if not results:                                                     # If the search doesn't succeed
+        print("No rooms found.")
+    else:                                                               # Prints the results
+        for room in results:
+            print(f"- Room number: {room['room_id']} | "f"- Room type: {room['room_name']} | "f"- Room cost: {room['room_cost']} € | "f"- Bed type: {room['rooms_beds']}");
