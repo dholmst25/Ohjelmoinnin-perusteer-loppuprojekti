19e3bd1299b9e31fdab9788a7af3f9c503b4827a
dholmst25 <daniel.holmstrom@edu.turkuamk.fi>
2026-04-20 14:47:42 +0300

    Room availability 1.1

--- DanielinPerseilyt/RoomAvailability.py
+++ DanielinPerseilyt/RoomAvailability.py
@@ -1,4 +1,8 @@
+print("+---------+-----------------+----------------------------------------+------+--------------+-----------+")
+print("| Room ID | Name            | Bed Configuration                      | Cost | Breakfast    | Available |")
+print("+---------+-----------------+----------------------------------------+------+--------------+-----------+")
 for room in rooms:
-  print("==== Available rooms are ====")
-  if room["room_availability"] == True:
-    print(room)
+  if room["room_availability"] == "Yes":
+    print(f"|Room {room['room_id']} | {room['room_name']} | {room['rooms_beds']} | {room['room_cost']}  | {room['room_breakfast']} | {room['room_availability']}       |")
+
+print("+---------+-----------------+----------------------------------------+------+--------------+-----------+")
