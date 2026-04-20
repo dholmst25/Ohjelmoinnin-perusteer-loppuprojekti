375c49e6b450423bd7d3073aadb348b7b5aeb0a6
dholmst25 <daniel.holmstrom@edu.turkuamk.fi>
2026-04-20 14:51:11 +0300

    Rooms list update

--- DanielinPerseilyt/HotelRooms.py
+++ DanielinPerseilyt/HotelRooms.py
@@ -4,3 +4,10 @@ rooms = [
     {"room_id": 103, "room_name": "Standard single room", "rooms_beds": "Single queen size bed", "room_cost": 60, "room_breakfast": False, "room_availability": True},
     {"room_id": 104, "room_name": "Family suite", "rooms_beds": "Three king size beds", "room_cost": 600, "room_breakfast": True, "room_availability": True},
 ]
+
+rooms = [
+    {"room_id": 101, "room_name": "Standard queen ", "rooms_beds": "One queen size bed                    ", "room_cost": 120, "room_breakfast": "Not Included", "room_availability": "No"}, 
+    {"room_id": 102, "room_name": "Standard king  ", "rooms_beds": "One king size bed                     ", "room_cost": 180, "room_breakfast": "Included    ", "room_availability": "Being cleaned"},
+    {"room_id": 103, "room_name": "Standard family", "rooms_beds": "Single king size and two single beds  ", "room_cost": 350, "room_breakfast": "Not Included", "room_availability": "Yes"},
+    {"room_id": 104, "room_name": "Exclusive suite", "rooms_beds": "Three king size beds in seperate rooms", "room_cost": 700, "room_breakfast": "Included    ", "room_availability": "Yes"},
+]
