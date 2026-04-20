f857e6a68943573e75ff1b9ede5d88b55488b250
dholmst25 <daniel.holmstrom@edu.turkuamk.fi>
2026-04-20 15:14:31 +0300

    Start Menu In main branch :3

---
+++ MainCodeFolder/StartMenu.py
@@ -0,0 +1,45 @@
+def startmenu():
+    print("┌───────────────────────────────────────────────┐")
+    print("|1 = View all available rooms                   |")
+    print("|2 = Book a room                                |") 
+    print("|3 = Check-out                                  |") 
+    print("|4 = Add room listing                           |") 
+    print("|5 = Remove room listing                        |")
+    print("|6 = Exit the program                           |")
+    print("└───────────────────────────────────────────────┘")
+    while True:
+        try:
+            StartDecision = int(input("Enter a number (1-6)"))
+            if 1 <= StartDecision <= 6: #Käytetään numeroita 1-6 aloittamaan eri osat ohjelmaa ja turvataan, jos käyttäjä antaa väärän numeron.
+                return StartDecision
+            else:
+                print("Incorrect value given. Try again.")
+        except ValueError:
+                print("Invalid number given. Please try again.")
+
+def exit_program():
+    print("Thank you for choosing us!")
+    return False
+
+while True:
+    choice = startmenu()
+
+    if choice == 1:
+        print(f"You have selected option {choice}")
+        print("Ho ho ho!")
+    elif choice == 2:
+        print(f"You have selected option {choice}")
+        print("Hahaa")
+    elif choice == 3:
+        print(f"You have selected option {choice}")
+        print("Hoo")
+    elif choice == 4:
+        print(f"You have selected option {choice}")
+        print("Sazam!")
+    elif choice == 5:
+        print(f"You have selected option {choice}")
+        print("Haha")
+    elif choice == 6:
+        print(f"You have selected option {choice}")
+        exit_program()
+        break
