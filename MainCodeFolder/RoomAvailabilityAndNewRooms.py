1465b886087c3227ba4756511614def15c7dcc1e
dholmst25 <daniel.holmstrom@edu.turkuamk.fi>
2026-04-20 15:11:00 +0300

    Room availability & new Rooms list

---
+++ MainCodeFolder/RoomAvailabilityAndNewRooms.py
@@ -0,0 +1,15 @@
+rooms = [
+    {"room_id": 101, "room_name": "Standard queen ", "rooms_beds": "One queen size bed                    ", "room_cost": 120, "room_breakfast": "Not Included", "room_availability": "No"}, 
+    {"room_id": 102, "room_name": "Standard king  ", "rooms_beds": "One king size bed                     ", "room_cost": 180, "room_breakfast": "Included    ", "room_availability": "Being cleaned"},
+    {"room_id": 103, "room_name": "Standard family", "rooms_beds": "Single king size and two single beds  ", "room_cost": 350, "room_breakfast": "Not Included", "room_availability": "Yes"},
+    {"room_id": 104, "room_name": "Exclusive suite", "rooms_beds": "Three king size beds in seperate rooms", "room_cost": 700, "room_breakfast": "Included    ", "room_availability": "Yes"},
+]
+
+print("+---------+-----------------+----------------------------------------+------+--------------+-----------+")
+print("| Room ID | Name            | Bed Configuration                      | Cost | Breakfast    | Available |")
+print("+---------+-----------------+----------------------------------------+------+--------------+-----------+")
+for room in rooms:
+  if room["room_availability"] == "Yes":
+    print(f"|Room {room['room_id']} | {room['room_name']} | {room['rooms_beds']} | {room['room_cost']}  | {room['room_breakfast']} | {room['room_availability']}       |")
+
+print("+---------+-----------------+----------------------------------------+------+--------------+-----------+")
