0834dfb76caf6716fc9552f931737dd4fb520ce3
markusi24 <255119525+markusi24@users.noreply.github.com>
2026-04-20 15:05:28 +0300

    Lisätty booking v0.2 MainCodeFolder kansioon

---
+++ MainCodeFolder/Room_booking.py
@@ -0,0 +1,14 @@
+import HotelRooms
+
+def book_room(room_id, rooms):
+    for room in rooms:
+        if room["room_id"] == room_id:
+            if room["room_availability"]:
+                room["room_availability"] = False
+                return "Room booked successfully!"
+            else:
+                return "Room already booked!"
+    return "Room not found!"
+
+
+print(book_room(104, HotelRooms.rooms))
