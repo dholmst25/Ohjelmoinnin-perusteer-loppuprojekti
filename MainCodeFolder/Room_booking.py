725c626c896c06bf6a9ef5324845128b82f70b32
markusi24 <255119525+markusi24@users.noreply.github.com>
2026-04-21 11:52:56 +0300

    Booking v1.2.1 (kieli vaihdettu englanniksi)

--- MainCodeFolder/Room_booking.py
+++ MainCodeFolder/Room_booking.py
@@ -3,7 +3,7 @@ def book_room(room_id, rooms):
         if room["room_id"] == room_id:
             if room["room_availability"]:
                 room["room_availability"] = False
-                return "Huone varattu onnistuneesti!"
+                return "Room booked successfully!"
             else:
-                return "Huone on jo varattu!"
-    return "Huonetta ei löytynyt!"
+                return "Room already booked!"
+    return "Room not found!"
