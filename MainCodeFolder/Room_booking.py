74ff1ce41a62d715f63ec1c879af977d96d78e27
markusi24 <255119525+markusi24@users.noreply.github.com>
2026-04-21 13:33:03 +0300

    Vaihdettiin koodia, jotta "room_availability" oikeasti vaihtuu tietokannassa ja korjattiin myös toimimaan uuteen tietokantaan.

--- MainCodeFolder/Room_booking.py
+++ MainCodeFolder/Room_booking.py
@@ -1,8 +1,8 @@
 def book_room(room_id, rooms):
     for room in rooms:
         if room["room_id"] == room_id:
-            if room["room_availability"]:
-                room["room_availability"] = False
+            if room["room_availability"] == "Yes":
+                room["room_availability"] = "No"
                 return "Room booked successfully!"
             else:
                 return "Room already booked!"
