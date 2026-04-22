89ef946f163d7a5ee2055a5c7295c3be19fece56
jesleh25 <jesse.lehtoranta@edu.turkuamk.fi>
2026-04-22 10:47:01 +0300

    Final version of Checkout

--- MainCodeFolder/Checkout.py
+++ MainCodeFolder/Checkout.py
@@ -1,10 +1,10 @@
 def Checkout(rooms, room_id):
     for room in rooms:
         if room["room_id"] == room_id:
-            if room["room_availability"]:
+            if room["room_availability"] == "Yes":
                 print("Huone on jo vapaa")
             else:
-                room["room_availability"] = True
+                room["room_availability"] == "No"
                 print(f"Checkout onnistui. Huone {room_id} on nyt vapaa.")
             return
     print("Huonetta ei löydy")
