734db3301dac893d4939ea55825aaf16197af987
markusi24 <255119525+markusi24@users.noreply.github.com>
2026-04-23 12:37:29 +0300

    Added elif with for "Being cleaned"

--- MainCodeFolder/Checkout.py
+++ MainCodeFolder/Checkout.py
@@ -5,12 +5,14 @@ def Checkout(rooms, room_id):
     for room in rooms:
         if room["room_id"] == room_id:
             if room["room_availability"] == "Yes":
-                print("Huone on jo vapaa")
+                print("Room already available!")
+            elif room["room_availability"] == "Being cleaned":
+                print("Room not available!")     
             else:
                 room["room_availability"] == "No"
                 room["room_availability"] = "Yes"
                 print(f"Checkout successful. Room {room_id} is now available.")
             return
-    print("Huonetta ei löydy")
+    print("Room not found!")
 
 
