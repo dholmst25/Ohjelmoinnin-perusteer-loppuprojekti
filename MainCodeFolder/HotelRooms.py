fff72f5943fd3701b539bc22ba29a6ba30882b44
dholmst25 <daniel.holmstrom@edu.turkuamk.fi>
2026-04-29 16:15:04 +0300

    Fix one Included text to not have extra spaces

--- MainCodeFolder/HotelRooms.py
+++ MainCodeFolder/HotelRooms.py
@@ -1,6 +1,6 @@
 rooms = [
     {"room_id": 101, "room_name": "Standard queen", "rooms_beds": "One queen size bed", "room_cost": 120, "room_breakfast": "Not Included", "room_availability": "No"}, 
-    {"room_id": 102, "room_name": "Standard king", "rooms_beds": "One king size bed", "room_cost": 180, "room_breakfast": "Included    ", "room_availability": "Being cleaned"},
+    {"room_id": 102, "room_name": "Standard king", "rooms_beds": "One king size bed", "room_cost": 180, "room_breakfast": "Included", "room_availability": "Being cleaned"},
     {"room_id": 103, "room_name": "Standard family", "rooms_beds": "Single king size and two single beds", "room_cost": 350, "room_breakfast": "Not Included", "room_availability": "Yes"},
     {"room_id": 104, "room_name": "Exclusive suite", "rooms_beds": "Three king size beds in seperate rooms", "room_cost": 700, "room_breakfast": "Included", "room_availability": "Yes"},
 ]
