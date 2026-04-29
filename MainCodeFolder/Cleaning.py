d6472473a493b40c870631cabb5cd9500eaffaa3
dholmst25 <daniel.holmstrom@edu.turkuamk.fi>
2026-04-29 16:38:57 +0300

    Oops now correct cleaning and checkout

--- MainCodeFolder/Cleaning.py
+++ MainCodeFolder/Cleaning.py
@@ -1,7 +1,7 @@
 import HotelRooms
 
 
-def Checkout(rooms):
+def Cleaning(rooms):
     try:
         room_id = int(input("Give the ID of the room you had:"))
     except ValueError:
@@ -11,11 +11,11 @@ def Checkout(rooms):
         if room["room_id"] == room_id:
             if room["room_availability"] == "Yes":
                 print("Room already available!")
-            elif room["room_availability"] == "Being cleaned":
-                print("Room not available!")
             elif room["room_availability"] == "No":
+                print("Room not available!")
+            elif room["room_availability"] == "Being cleaned":
                 room["room_availability"] = "Yes"
-                print(f"Checkout successful. Room {room_id} is now available.")
+                print(f"Checkout successful. Room {room_id} is now cleaned and available.")
             break
     else:           
         print("Room not found!")
